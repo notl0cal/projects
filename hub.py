1ed5188c2f4f4745bc7c6b3378a4381a402a1a5d
notl0cal <forchancee@gmail.com>
2021-11-25 11:48:21 -0600

    added reset func

--- hub.py
+++ hub.py
@@ -3,9 +3,10 @@
 import sys
 import os
 import math
+import time
 from sys import platform
 from datetime import datetime
-#global declarations
+#global variable declarations
 y = ["Y", "y", "YES", "yes"]
 n = ["N", "n", "NO", "no"]
 today = datetime.today()
@@ -18,6 +19,20 @@ def clear():
         os.system("cls")
 def exit():
     sys.exit("Exiting...")
+def resetting(times:int):
+    animation = "|/-\\"
+    for i in range(times):
+        time.sleep(0.1)
+        sys.stdout.write("\r" + "Resetting... " + animation[i % len(animation)])
+        sys.stdout.flush()
+    print("Finished!")
+def loading(times:int):
+    animation = "|/-\\"
+    for i in range(times):
+        time.sleep(0.1)
+        sys.stdout.write("\r" + "Doing Maths... " + animation[i % len(animation)])
+        sys.stdout.flush()
+    print("Finished!")
 #main function
 def intro():
     c = 1
@@ -58,6 +73,9 @@ def pChange():
     pc2 = float(input("Enter the final price: "))
     pc2 = (pc1 - pc2) / pc1
     result = "%.0f%%" % (-100 * pc2)
+    clear()
+    loading(20)
+    clear()
     print(result)
     f = input("Do you want to save this file? (Y/N)")
     if f in y:
@@ -65,6 +83,7 @@ def pChange():
             file.write(date + "\n" + result )
     else:
         clear()
+        resetting(15)
         intro()
 def dca():
     dca1 = [float(x) for x in input("Please enter dollar amounts with spaces inbetween.\n$: ").split()]
@@ -72,6 +91,8 @@ def dca():
     dca1.sort()
     values = "\nValues: $" + str(dca1)
     result = "\nAverage: $" + str(dca2) + "\n"
+    clear()
+    loading(20)
     clear()
     print(values)
     print(result)
@@ -81,17 +102,19 @@ def dca():
             file.write(date + values + result)
     else:
         clear()
+        resetting(15)
         intro()
 def eMath():
     ticker = str(input("Please enter the ticker:\n: "))
     em1 = float(input("Enter your entry point in dollars:\n$: "))
-    eVol = float(input("Please enter number of units purchased:\nU: "))
+    eVol = float(input("Please enter number of " + ticker.upper() + " purchased:\n#: "))
     print("""
-     / Stop : Take /
-    1.) 3:6
-    2.) 6:12
-    """)
-    eRatio = int(input("Please select a trade ratio: "))
+How would you like to setup your trade?
+
+        1.) 3:6
+        2.) 6:12
+""")
+    eRatio = int(input("Please select an option: "))
     if eRatio == 1:
         emTake = (em1 * 0.06) + em1
         emStop = em1 - (em1 * 0.03)
@@ -107,10 +130,13 @@ def eMath():
         emLoss = (em1 - emStop) * eVol
         eFut = emProfit + ePos
     else:
-        input("Not a valid option. Resetting...")
+        print("Not a valid option...")
+        resetting(15)
         clear()
         eMath()
     result = "\nTicker: " + str(ticker.upper()) + "\n\nEntry: $" + str(em1) + "\nPosition: $" + str(ePos) + "\nFuture Position: $" + str(eFut) + "\n\nTake: $" + str(emTake) + "\nStop: $" + str(emStop) + "\n\nProfit: $" + str(emProfit) + "\nLoss: $" + str(emLoss) + "\n"
+    clear()
+    loading(25)
     clear()
     print(result)
     f = input("Do you want to save this file? (Y/N)")
@@ -119,6 +145,7 @@ def eMath():
             file.write(date + result)
     else:
         clear()
+        resetting(15)
         intro()
 #main function call
 intro()
