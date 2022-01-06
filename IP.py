3d29534a3c1d2a7f4c8c731b7c60d808053ebb87
BuildTools <unconfigured@null.spigotmc.org>
2022-01-05 20:36:52 -0600

    basic list of harvested ip's and their percentage of occurences in a file

--- IP.py
+++ IP.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+import matplotlib
 import sys
 import re
 
 def harvester(file):
-    for i in file.readlines():
-        ip = re.findall("\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b")
+    d = {}
+    i = file.read()
+    count = 0
+    ips = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})', i)
+    if ips:
+        for ip in ips:
+            count += 1
+            if ip in d:
+                d[ip] += 1
+            else:
+                d[ip] = 1
+    for key, value in sorted(d.items()):
+        perc = ((value / count) * 100)
+        print(key + " - " + str(round(perc, 2))+ "%")
+    print("Number of IP Addresses: " + str(count))
+
+
+            
+            
+        
 
 def main():
     args = sys.argv[1]
