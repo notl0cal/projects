3764b96e10dda6324048c7a7d9e8d41ed69fdfa1
BuildTools <unconfigured@null.spigotmc.org>
2022-01-05 22:04:26 -0600

    finished

--- IP.py
+++ IP.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-import matplotlib
+import matplotlib.pyplot as plt
 import sys
 import re
 
 def harvester(file):
     d = {}
+    ipsa = []
+    ipsp = []
     i = file.read()
     count = 0
     ips = re.findall(r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})', i)
@@ -17,7 +19,29 @@ def harvester(file):
                 d[ip] = 1
     for key, value in sorted(d.items()):
         perc = ((value / count) * 100)
-        print(key + " - " + str(round(perc, 2))+ "%")
+        ipsa.append(key)
+        ipsp.append(perc)
+    fig, ax = plt.subplots(figsize=(16, 9))
+    ax.barh(ipsa, ipsp)
+    for s in ['top', 'bottom', 'left', 'right']:
+        ax.spines[s].set_visible(False)
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_tick_params(pad = 5)
+    ax.yaxis.set_tick_params(pad = 10)
+    ax.grid(b = True, color ='grey',
+        linestyle ='-.', linewidth = 0.5,
+        alpha = 0.2)
+    ax.invert_yaxis()
+    for i in ax.patches:
+        plt.text(i.get_width()+0.2, i.get_y()+0.6,
+                str(round((i.get_width()), 2)) + "%",
+                fontsize = 10, fontweight ='bold',
+                color ='grey')
+    ax.set_title('Harvested IP Addresses',
+             loc ='left', )
+    plt.show()
+        #print(key + " - " + str(round(perc, 2))+ "%")
     print("Number of IP Addresses: " + str(count))
 
 
